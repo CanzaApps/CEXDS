f9c82a1160c9a3aa6592c48d7f3315038b57b820
marvikomo <marvikomo@gmail.com>
2024-01-03 15:11:24 +0100

    update

--- contracts/cexDS/CEXDefaultSwap.sol
+++ contracts/cexDS/CEXDefaultSwap.sol
@@ -352,6 +352,7 @@ contract CEXDefaultSwap is DateTime, Ownable, ICreditDefaultSwap {
 
     function _transferTo(uint256 _amount, address _user) internal {
         bool transferSuccess = IERC20(currency).transfer(_user, _amount);
+
         if (!transferSuccess) revert();
     }
 }
